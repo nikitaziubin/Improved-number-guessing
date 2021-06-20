0873d2072e7deb0ec72c59b174a54d6f7d54df42
РобоАрт <49118839+nikitaziubin@users.noreply.github.com>
2021-06-20 09:48:26 +0000

    added number is higher process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-number = random.randint(1, 5)
+number = random.randint(1, 10)
 count = 0
 guess = 0
 while guess != number:
@@ -8,5 +8,9 @@ while guess != number:
     guess = input('Guess a number between 1 and 5: ')
     if guess.isnumeric():
         guess = int(guess)
+    elif guess > number:
+      print("Your guess is too high, try again!")  
+    elif guess < number:  
+        print("Your guess is too low, try again!") 
 else:
     print(f'You guessed it in {count} tries!')
