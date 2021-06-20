ecc71a8f0283ba66a3b7cee23307ac18febbf81b
РобоАрт <49118839+nikitaziubin@users.noreply.github.com>
2021-06-20 10:17:24 +0000

    added max_allowed_nmber

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import random
 
-number = random.randint(1, 10)
+max_allowed_nmber = 10
+number = random.randint(1, max_allowed_nmber)
 count = 0
 guess = 0
 while guess != number:
     count += 1
-    guess = input('Guess a number between 1 and 5: ')
+    guess = input('Guess a number between 1 and ' +  str(max_allowed_nmber) + ':')
+
     if guess.isnumeric():
         guess = int(guess)
-    elif guess > number:
-      print("Your guess is too high, try again!")  
+    if guess > number:
+            print("Your guess is too high, try again!")  
     elif guess < number:  
-        print("Your guess is too low, try again!") 
+           print("Your guess is too low, try again!") 
 else:
     print(f'You guessed it in {count} tries!')
